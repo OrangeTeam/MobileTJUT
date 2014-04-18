a0664110a24f7298fcb8b4f74428496cad5e6525
Bai-Jie <BaiJie1991@gmail.com>
2014-04-18 13:04:36 +0800

    让MainActivity处理它添加的MenuItem

--- app/src/main/java/org/orange/mobiletjut/app/MainActivity.java
+++ app/src/main/java/org/orange/mobiletjut/app/MainActivity.java
@@ -1,22 +1,18 @@
 package org.orange.mobiletjut.app;
 
-import android.app.Activity;
-
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity
@@ -100,6 +96,12 @@ public class MainActivity extends Activity
         if (id == R.id.action_settings) {
             return true;
         }
+
+        if (id == R.id.action_example) {
+            Toast.makeText(this, "Example action.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
